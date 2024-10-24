2d9e7b3cc11a22ce809fc70663e4db00ef0266b3
InsafKarimov <123745461+InsafKarimov@users.noreply.github.com>
2024-10-24 20:37:40 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import Card from './components/Card';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
-function App() {
-  return <div className="wrapper clear">
-  <Drawer />
-  <Header />
-  <div className="content p-40">
-    <div className="d-flex align-center justify-between mb-40">
-      <h1>Все кроссовки</h1>
-      <div className="search-block d-flex align-center">
-        <img src="/img/search.svg" alt="Search" />
-        <input placeholder="Поиск..." />
-      </div>
-    </div>
-    <div className="d-flex justify-between">
-      <Card />
-      
-    </div>
+import AppContext from './context';
 
-  </div>
-  </div>;
+import Home from './pages/Home';
+
+
+function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [cartOpened, setCartOpened] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+        const [cartResponse, itemsResponse] = await Promise.all([
+          axios.get('https://67042f96ab8a8f892733388e.mockapi.io/cart'),
+          axios.get('https://67042f96ab8a8f892733388e.mockapi.io/items'),
+        ]);
+
+        setIsLoading(false);
+        setCartItems(cartResponse.data);
+        setItems(itemsResponse.data);
+      } catch (error) {
+        alert('Ошибка при запросе данных ;(');
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const onAddToCart = async (obj) => {
+    try {
+      const findItem = cartItems.find((item) => Number(item.parentId) === Number(obj.id));
+      if (findItem) {
+        setCartItems((prev) => prev.filter((item) => Number(item.parentId) !== Number(obj.id)));
+        await axios.delete(`https://67042f96ab8a8f892733388e.mockapi.io/cart/${findItem.id}`);
+      } else {
+        setCartItems((prev) => [...prev, obj]);
+        const { data } = await axios.post('https://67042f96ab8a8f892733388e.mockapi.io/cart', obj);
+        setCartItems((prev) =>
+          prev.map((item) => {
+            if (item.parentId === data.parentId) {
+              return {
+                ...item,
+                id: data.id,
+              };
+            }
+            return item;
+          }),
+        );
+      }
+    } catch (error) {
+      alert('Ошибка при добавлении в корзину');
+      console.error(error);
+    }
+  };
+
+  const onRemoveItem = (id) => {
+    try {
+      axios.delete(`https://67042f96ab8a8f892733388e.mockapi.io/cart/${id}`);
+      setCartItems((prev) => prev.filter((item) => Number(item.id) !== Number(id)));
+    } catch (error) {
+      alert('Ошибка при удалении из корзины');
+      console.error(error);
+    }
+  };
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const isItemAdded = (id) => {
+    return cartItems.some((obj) => Number(obj.parentId) === Number(id));
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        items,
+        cartItems,
+        isItemAdded,
+        onAddToCart,
+        setCartOpened,
+        setCartItems,
+      }}>
+      <div className="wrapper clear">
+        <Drawer
+          items={cartItems}
+          onClose={() => setCartOpened(false)}
+          onRemove={onRemoveItem}
+          opened={cartOpened}
+        />
+
+        <Header onClickCart={() => setCartOpened(true)} />
+
+        <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              items={items}
+              cartItems={cartItems}
+              searchValue={searchValue}
+              setSearchValue={setSearchValue}
+              onChangeSearchInput={onChangeSearchInput}
+              onAddToCart={onAddToCart}
+              isLoading={isLoading}
+            />
+          }
+          exact
+        />
+      </Routes>
+
+      </div>
+    </AppContext.Provider>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,40 @@
-function Header() {
-    return <header className="d-flex justify-between align-center p-40">
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { useCart } from '../hooks/useCart';
+
+function Header(props) {
+  const { totalPrice } = useCart();
+
+  return (
+    <header className="d-flex justify-between align-center p-40">
+      <Link to="/">
         <div className="d-flex align-center">
-        <img width={40} height={40} src="/img/logo.png" alt="Logo"/>
-        <div>
-            <h3>REACT SNEAKERS</h3>
+          <img width={40} height={40} src="img/logo.png" alt="Logotype" />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
             <p className="opacity-5">Магазин лучших кроссовок</p>
+          </div>
         </div>
-        </div>
-        <ul className="d-flex align-center">
-        <li className="mr-30 d-flex align-center">
-            <img width={18} height={18} src="/img/card.svg" className="mr-10" alt="Цена"/>
-            <span>1205 руб.</span>
+      </Link>
+      <ul className="d-flex">
+        <li onClick={props.onClickCart} className="mr-30 cu-p">
+          <img width={18} height={18} src="img/cart.svg" alt="Корзина" />
+          <span>{totalPrice} руб.</span>
         </li>
-        <li className="mr-30 d-flex align-center">
-            <img width={18} height={18} src="/img/favorite.svg" className="mr-10" alt="Закладки"/>
-            <span>Закладки</span>
+        <li className="mr-20 cu-p">
+          <Link to="/favorites">
+            <img width={18} height={18} src="img/heart.svg" alt="Закладки" />
+          </Link>
         </li>
-        <li className="mr-30 d-flex align-center">
-            <img width={18} height={18} src="/img/user.svg" className="mr-10" alt="Профиль" />
-            <span>Профиль</span>
+        <li>
+          <Link to="/orders">
+            <img width={18} height={18} src="img/user.svg" alt="Пользователь" />
+          </Link>
         </li>
-        </ul>
+      </ul>
     </header>
+  );
 }
 
 export default Header;
